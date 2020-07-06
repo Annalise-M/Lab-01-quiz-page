f63bfd0a311f7d1a211a1613840367f93bc0e655
Annalise Murphy <nali247@gmail.com>
2020-07-06 15:54:30 -0700

    eventListener added with working first prompt response

---
+++ app.js
@@ -0,0 +1,11 @@
+//import functions and grab DOM elements
+
+const myButton = document.getElementById('quiz-button');
+
+// initialize state
+
+// set event listeners to update state and DOM
+myButton.addEventListener('click', () => {
+    prompt('what is your name');
+    // console.log('hello');
+});

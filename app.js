4d7fb885bf9b1749bdc05dd4f064e140148474d6
Annalise Murphy <nali247@gmail.com>
2020-07-06 16:00:35 -0700

    logging out for 3 prompted questions and responses

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const myButton = document.getElementById('quiz-button');
 
 // set event listeners to update state and DOM
 myButton.addEventListener('click', () => {
-    prompt('what is your name');
-    // console.log('hello');
+
+    const name = prompt('what is your name?');
+    const response1 = prompt('Is the sky blue in the photo?');
+    const response2 = prompt('Is there a RV in the pic?');
+    const response3 = prompt('Are there any birds visible in the pic?');
+
+    console.log(name, response1, response2, response3);
 });
